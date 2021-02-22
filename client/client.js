8e593e9708facd8f054b9abc8980540c8080f868
Varmeusz <adam.w1998@gmail.com>
2021-02-22 14:21:30 +0100

    added image upload/ get methods

--- client/client.js
+++ client/client.js
@@ -1,9 +1,9 @@
-const API_URL = 'http://localhost:5000/nurupo';
+const API_URL = 'http://localhost:5000';
 const form = document.querySelector('form');
 const loadingElement = document.querySelector('.loading');
 loadingElement.style.display = "none";
 
-form.addEventListener('submit', (e) =>{
+form.addEventListener('submit', async (e) =>{
     e.preventDefault();
     const formData = new FormData(form);
     const name = formData.get("name");
@@ -14,11 +14,45 @@ form.addEventListener('submit', (e) =>{
     };
     form.style.display = 'none';
     loadingElement.style.display = '';
-    fetch(API_URL, {
+    fetch(API_URL+"/nurupo", {
         method: 'POST',
-        body: JSON.stringify(post),
+        body: (formData),
         headers:{
-            'Content-Type': 'application/json',
+            
         },
-    });
+    }).then(response => response.json())
+      .then(createdNurupo => {
+          var img = document.createElement("img");
+          img.src = "http://localhost:5000/uploads?id="+createdNurupo;
+          document.querySelector(".nurupos").prepend(img);
+          img = null;
+          setTimeout(()=> {
+            loadingElement.style.display = 'none';
+            form.style.display = '';}, 100);
+      })
 });
+loadNurupos = (e) =>{
+    var ids = [];
+    fetch(API_URL+"/nurupos", {
+        method: 'GET',
+    }).then(response => response.json())
+      .then(nuruposArr => {
+        ids = nuruposArr;
+        console.log(ids);
+        ids.forEach(element => {
+            var container = document.createElement("div");
+            var div = document.createElement("h3");
+            div.innerHTML = element[0];
+            container.appendChild(div);
+            var img = document.createElement("img");
+            img.src = "http://localhost:5000/uploads?id="+element[1];
+            container.appendChild(img);
+            img = null; 
+            div = document.createElement("small");
+            div.innerHTML = element[2];
+            container.appendChild(div);
+            document.querySelector(".nurupos").appendChild(container);
+        });
+    })
+};
+loadNurupos();
--- server/index.js
+++ server/index.js
@@ -1,30 +1,110 @@
 const express = require('express');
 const cors = require('cors');
+const monk = require('monk');
 const app = express();
+var path = require("path");
+const db = monk('localhost/nurupo');
+const nurupos = db.get('nurupos');
+
+const fs = require('fs');
+const stream = require('stream');
+
+const formidable = require('formidable');
+const { time } = require('console');
+const { create } = require('domain');
 app.use(cors());
-app.use(express.json());
+
 app.get('/', (req, res) => {
     res.json({
         message: 'Nurupo!'
     })
 });
-isValidTweet = (tweet) => {
+isValidRequest = (req) => {
     return tweet.name  && tweet.name.toString().trim()  !== '' &&
         tweet.content && tweet.content.toString().trim() !== '';
 }
 
-app.post('/nurupo', (req, res) => {
-    if(isValidTweet(req.body)){
-        const tweet = {
-            name: req.body.name.toString(),
-            content: req.body.content.toString()
-        };
-        console.log(tweet);
-    }else{
-        res.status(422);
-        res.json({message: 'Specify name and content'});
-    }
+app.post('/nurupos', (req, res) => {
+    var form = new formidable.IncomingForm(),//.parse(req, (err, fields, files) => {
+    files = [],
+    fields = [];
+    form.on('fileBegin', (name, file) => {
+        file.path = __dirname + '/uploads/' + file.name
+    });
+    form.on('field', (field, value) => {
+        fields.push([field, value]);
+    });
+    form.on('file', (field, file) => {
+        console.log(file.path);
+        files.push([field, file]);
+    })
+    form.on('end', () => {
+        //do something with files and fields
+
+        //save to db
+        
+        res.status(201);
+        res.end();
+    });
+    form.parse(req);
 });
+app.post('/nurupo', (req, res) => {
+    var form = new formidable.IncomingForm(),
+    files = [],
+    fields = [];
+    form.maxFileSize = 10000000;
+    form.on('fileBegin', (name, file) => {
+        file.path = __dirname + '/uploads/images/' + file.name
+    });
+    form.on('field', (field, value) => {
+        fields.push([field, value]);
+    });
+    form.on('file', (field, file) => {
+        files.push([field, file]);
+    })
+    form.on('end', () => {
+        //do something with files and fields
+
+        //save to db
+        const nurupo = {
+            name : fields[0][1],
+            file : files[0][1].path,
+            created : new Date()
+        }
+        nurupos
+            .insert(nurupo)
+            .then(createdNurupo => {
+                res.json(createdNurupo._id);
+            })
+    });
+    form.parse(req);
+});
+
+app.get("/nurupos", (req,res) => {
+    nurupos
+        .find({}, {limit: 10, sort: {"_id":-1}})
+        .then(nuruposFound => {
+            var ids = nuruposFound.reduce((r,e) => {
+                r.push([e.name, e._id, e.created]);
+                return r;
+            }, []);
+            res.json(ids);
+        })
+});
+app.get("/uploads", (req, res) => {
+    nurupos
+        .find( {'_id': req.query.id})
+        .then(foundNurupo => {
+            // console.log(foundNurupo[0].file);
+            // console.log(req.query.id);
+            res.sendFile(path.resolve(foundNurupo[0].file));
+        })
+});
+// app.get('/hutaogears.png', (req, res) => {
+//     console.log(path.resolve("uploads/images/hutaogears.png"));
+//     res.sendFile(path.resolve("uploads/images/hutaogears.png"));
+// });
+// app.use(express.static('uploads'));
 
 app.listen(5000, () => {
     console.log('Listening on http://localhost:5000');
