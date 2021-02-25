7b04e00ceeec94686c97cd3cf8e7c4caf55fe2eb
varmeusz <adam@awaclawczyk.xyz>
2021-02-25 16:49:37 +0000

    changed API_URL

--- client/client.js
+++ client/client.js
@@ -1,5 +1,4 @@
-//const API_URL = 'http://localhost:5001';
- const API_URL = 'https://awaclawczyk.xyz/nurupo';
+const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:5001' :'https://awaclawczyk.xyz/nurupo';
 const form = document.querySelector('form');
 const loadingElement = document.querySelector('.loading');
 loadingElement.style.display = "none";
